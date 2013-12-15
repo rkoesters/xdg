20412791d7110aa57e012eb6047fa1836e606953
Ryan Koesters <rmkoesters@gmail.com>
2013-12-15 04:29:08 -0600

    Added ini file format.

---
+++ ini/ini.go
@@ -0,0 +1,45 @@
+// Package ini implements the ini file format that is used in many of
+// the xdg specs.
+package ini
+
+import (
+	"io"
+	"bufio"
+	"strings"
+	"errors"
+)
+
+// File is a map of a map of strings. The first string is the header
+// section and the second is the key.
+type File map[string]map[string]string
+
+// New creates a new File and returns it.
+func New(r io.Reader) (File, error) {
+	file := make(File)
+	hdr := "default"
+	file[hdr] = make(map[string]string)
+
+	sc := bufio.NewScanner(r)
+	for sc.Scan() {
+		line := strings.TrimSpace(sc.Text())
+		switch {
+		case len(line) == 0:
+			// Empty line.
+		case strings.HasPrefix(line, "#"):
+			// Comment.
+		case line[0:1] == "[" && line[len(line)-1:] == "]":
+			// New section.
+			hdr = line[1:len(line)-1]
+			file[hdr] = make(map[string]string)
+		case strings.Contains(line, "="):
+			// Key=Value pair.
+			p := strings.SplitN(line, "=", 2)
+			p[0] = strings.TrimSpace(p[0])
+			p[1] = strings.TrimSpace(p[1])
+			file[hdr][p[0]] = p[1]
+		default:
+			return nil, errors.New("invalid format")
+		}
+	}
+	return file, nil
+}
---
+++ ini/ini_test.go
@@ -0,0 +1,51 @@
+package ini
+
+import (
+	"testing"
+	"strings"
+)
+
+const testFile = `
+# This right here is a test file.
+[Header 1]
+key=value
+	# This line is used to test extraneous white space.
+	cat = dog 
+[Header 2]
+# This line tests for extra equal signs.
+man = bear = pig
+`
+
+func TestNew(t *testing.T) {
+	r := strings.NewReader(testFile)
+	file, err := New(r)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(file)
+
+	if file["Header 1"]["key"] != "value" {
+		t.Error("basic usage")
+	}
+	if file["Header 1"]["cat"] != "dog" {
+		t.Error("whitespace")
+	}
+	if file["Header 2"]["man"] != "bear = pig" {
+		t.Error("equal signs")
+	}
+}
+
+const testFileInvalid = `
+# This example will have an invalid line.
+[Header 1]
+key=value
+hello world!
+`
+
+func TestInvalid(t *testing.T) {
+	r := strings.NewReader(testFileInvalid)
+	_, err := New(r)
+	if err == nil {
+		t.Fail()
+	}
+}
