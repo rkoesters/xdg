2b4d42190ded1cabac387fdb989e613d139774b8
Ryan Koesters <rmkoesters@gmail.com>
2018-01-30 11:44:25 -0600

    Continue adjusting keyfile package to name change.

--- keyfile/keyfile.go
+++ keyfile/keyfile.go
@@ -13,18 +13,18 @@ import (
 	"strings"
 )
 
-// Map is a map of a map of strings. The first string is the header
+// KeyFile is a map of a map of strings. The first string is the header
 // section and the second is the key.
-type Map struct {
-	M map[string]map[string]string
+type KeyFile struct {
+	m map[string]map[string]string
 }
 
-// New creates a new Map and returns it.
-func New(r io.Reader) (*Map, error) {
-	m := new(Map)
-	m.M = make(map[string]map[string]string)
+// New creates a new KeyFile and returns it.
+func New(r io.Reader) (*KeyFile, error) {
+	kf := new(KeyFile)
+	kf.m = make(map[string]map[string]string)
 	hdr := ""
-	m.M[hdr] = make(map[string]string)
+	kf.m[hdr] = make(map[string]string)
 
 	sc := bufio.NewScanner(r)
 	for sc.Scan() {
@@ -37,35 +37,35 @@ func New(r io.Reader) (*Map, error) {
 		case line[0:1] == "[" && line[len(line)-1:] == "]":
 			// New section.
 			hdr = line[1 : len(line)-1]
-			m.M[hdr] = make(map[string]string)
+			kf.m[hdr] = make(map[string]string)
 		case strings.Contains(line, "="):
 			// Key=Value pair.
 			p := strings.SplitN(line, "=", 2)
 			p[0] = strings.TrimSpace(p[0])
 			p[1] = strings.TrimSpace(p[1])
-			m.M[hdr][p[0]] = p[1]
+			kf.m[hdr][p[0]] = p[1]
 		default:
 			return nil, errors.New("invalid format")
 		}
 	}
-	return m, nil
+	return kf, nil
 }
 
 // String returns the value with the given group and key. This function
-// is here because underlying data structure of Map may change.
-func (m *Map) String(g, k string) string {
-	return m.M[g][k]
+// is here because underlying data structure of KeyFile may change.
+func (kf *KeyFile) String(g, k string) string {
+	return kf.m[g][k]
 }
 
 // Bool returns the value as a bool.
-func (m *Map) Bool(g, k string) bool {
-	b, _ := strconv.ParseBool(m.String(g, k))
+func (kf *KeyFile) Bool(g, k string) bool {
+	b, _ := strconv.ParseBool(kf.String(g, k))
 	return b
 }
 
 // List returns the value as a slice of strings.
-func (m *Map) List(g, k string) []string {
-	l := strings.Split(m.String(g, k), ";")
+func (kf *KeyFile) List(g, k string) []string {
+	l := strings.Split(kf.String(g, k), ";")
 	for i := 0; i < len(l); i++ {
 		if l[i] == "" {
 			l = append(l[:i], l[i+1:]...)
