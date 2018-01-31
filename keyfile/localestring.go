3002b6efd72ac1a7038748f20d89ef261cfa5e7f
Ryan Koesters <rmkoesters@gmail.com>
2018-01-30 21:02:59 -0600

    Add LocaleStringList().

--- keyfile/localestring.go
+++ keyfile/localestring.go
@@ -22,5 +22,20 @@ func (kf *KeyFile) LocaleStringWithLocale(g, k string, l *Locale) (string, error
 	return kf.String(g, k)
 }
 
-// TODO LocaleStringList
-// TODO LocaleStringListWithLocale
+// LocaleStringList returns a slice of strings for group 'g' and key
+// 'k'.
+func (kf *KeyFile) LocaleStringList(g, k string) ([]string, error) {
+	return kf.LocaleStringListWithLocale(g, k, DefaultLocale())
+}
+
+// LocaleStringListWithLocale returns a slice of strings for group 'g',
+// key 'k', and locale 'l'.
+func (kf *KeyFile) LocaleStringListWithLocale(g, k string, l *Locale) ([]string, error) {
+	for _, locale := range l.Variants() {
+		key := fmt.Sprintf("%v[%v]", k, locale)
+		if kf.ValueExists(g, key) {
+			return kf.StringList(g, key)
+		}
+	}
+	return kf.StringList(g, k)
+}
