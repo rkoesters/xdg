4c260fc5d9b8cac58dc273a25253340d9dd4252d
Ryan Koesters <rmkoesters@gmail.com>
2018-10-22 13:49:55 -0500

    Remove warning from desktop/doc.go.

--- desktop/doc.go
+++ desktop/doc.go
@@ -1,7 +1,4 @@
 // Package desktop implements the Desktop Entry Spec. For more
 // information, please see the spec:
 // http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
-//
-// WARNING: The API for this package is not yet stable. Please avoid
-// using this package until this warning is removed.
 package desktop
