01d9f8b092bd8d04db0f1417ba0999a886316c37
Ryan Koesters <rmkoesters@gmail.com>
2018-10-22 22:42:19 -0500

    Add link to trash spec.

--- trash/doc.go
+++ trash/doc.go
@@ -1,4 +1,6 @@
-// Package trash provides easy access to the trash bin.
+// Package trash provides easy access to the trash bin. For more
+// information, see the trash spec:
+// https://standards.freedesktop.org/trash-spec/trashspec-latest.html
 //
 // TODO: Make New work.
 package trash
