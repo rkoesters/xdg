291d166a02595e356c071dcbdf40fce3e253d987
Ryan Koesters <rmkoesters@gmail.com>
2018-11-14 12:57:51 -0600

    Add keyfile.TestLocaleStringList.

--- keyfile/localestring_test.go
+++ keyfile/localestring_test.go
@@ -1,6 +1,7 @@
 package keyfile
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -10,6 +11,9 @@ const localeStringExample = `
 Key=Value 0
 Key[en_US]=Value 1
 Key[en_UK]=Value 2
+List=one;two;three;
+List[en_US]=four;five;six;
+List[en_UK]=seven;eight;nine;
 `
 
 func TestLocaleString(t *testing.T) {
@@ -54,6 +58,52 @@ func TestLocaleString(t *testing.T) {
 		t.Error(err)
 	}
 	if s != "Value 2" {
+		t.Errorf("expected=Value 2 real=%v", s)
+	}
+}
+
+func TestLocaleStringList(t *testing.T) {
+	kf, err := New(strings.NewReader(localeStringExample))
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(kf)
+
+	_, err = kf.LocaleStringList("Header 1", "List")
+	if err != nil {
+		t.Error(err)
+	}
+
+	s, err := kf.LocaleStringListWithLocale("Header 1", "List", &Locale{})
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(s, []string{"one", "two", "three"}) {
+		t.Errorf("expected=Value 0 real=%v", s)
+	}
+
+	locale, err := ParseLocale("en_US")
+	if err != nil {
+		t.Error(err)
+	}
+	s, err = kf.LocaleStringListWithLocale("Header 1", "List", locale)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(s, []string{"four", "five", "six"}) {
+		t.Errorf("expected=Value 1 real=%v", s)
+	}
+
+	locale, err = ParseLocale("en_UK")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(locale)
+	s, err = kf.LocaleStringListWithLocale("Header 1", "List", locale)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(s, []string{"seven", "eight", "nine"}) {
 		t.Errorf("expected=Value 2 real=%v", s)
 	}
 }
